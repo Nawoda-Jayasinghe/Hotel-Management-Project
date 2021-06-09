7a008763bdfc21548c068babd6eaa43f5a41ba46
Nawoda Jayasinghe <nawoda.jayasinghe98@gmail.com>
2021-06-09 21:52:09 +0530

    Second Comit

--- Hotel Management System/Reciptionist.cs
+++ Hotel Management System/Reciptionist.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Hotel_Management_System
 {
-    public partial class Form1 : Form
+    public partial class Reciptionist : Form
     {
-        public Form1()
+        public Reciptionist()
         {
             InitializeComponent();
         }
@@ -28,6 +28,31 @@ namespace Hotel_Management_System
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox18_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label22_Click(object sender, EventArgs e)
         {
 
         }
